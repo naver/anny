d846bd4df30cdc3baba34ba5a09636e175030d71
Romain Brégier <romain.bregier@naverlabs.com>
2026-07-24 18:42:03 +0200

    feat: dynamic version number

--- src/anny/__init__.py
+++ src/anny/__init__.py
@@ -5,3 +5,10 @@ from anny.models import *
 from anny.anthropometry import Anthropometry
 from anny.parameters_regressor import ParametersRegressor
 from anny.keypoints import KeypointsRegressor
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("anny")
+except importlib.metadata.PackageNotFoundError:
+    # The source directory was imported without installing the package.
+    __version__ = "0.0.0+unknown"
